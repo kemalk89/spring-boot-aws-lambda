1db902b160c41cc275ec9c6f1789d9b16ece222c
Kemal <k.kemal.1989@gmail.com>
2022-09-01 06:59:09 +0200

    Initial

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class Todo {
+	
+	private String title;
+	private String description;
+
+}
---
+++ src/main/java/com/example/demo/TodoController.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/todos")
+public class TodoController {
+
+	@GetMapping
+	public List<Todo> getTodos() {
+		log.info("Running getTodos...");
+		
+		var todo = new Todo();
+		todo.setTitle("Testtitle");
+		todo.setDescription("This is a test description.");
+		
+		return Arrays.asList(todo);
+	}
+	
+	@GetMapping("/{todoId}")
+	public Todo getTodo(@PathVariable String todoId) {
+		log.info("Running getTodo where ID is " + todoId);
+		
+		var todo = new Todo();
+		todo.setTitle("Testtitle");
+		todo.setDescription("This is a test description.");
+		
+		return todo;
+	}
+	
+}
