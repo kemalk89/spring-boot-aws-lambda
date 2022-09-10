a22219a596101982b222752e3b2f180e7c4aa815
Kemal <k.kemal.1989@gmail.com>
2022-09-10 11:37:02 +0200

    Timer for debug purpose.

--- src/main/java/com/examples/todo/lambda/StreamLambdaHandler.java
+++ src/main/java/com/examples/todo/lambda/StreamLambdaHandler.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.internal.testutils.Timer;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
@@ -28,6 +29,11 @@ public class StreamLambdaHandler implements RequestStreamHandler {
             e.printStackTrace();
             throw new RuntimeException("Could not initialize Spring Boot application", e);
         }
+    }
+
+    public StreamLambdaHandler() {
+        // we enable the timer for debugging. This SHOULD NOT be enabled in production.
+        Timer.enable();
     }
 
     @Override
